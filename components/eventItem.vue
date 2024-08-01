b51871d3c62170f78a4beaf6962260bddecc4665
dahi.hichem <dahi.hichem@smeetz.com>
2024-08-01 15:27:04 +0100

    add documentation to eventItem component & formatDateAndTime function

--- components/eventItem.vue
+++ components/eventItem.vue
@@ -31,10 +31,15 @@
 import type { Event } from '~/models/event';
 
 const props = defineProps<{event: Event}>()
-
 const active = ref(false)
 
-const formattedDateAndTimeHtml = computed(() => {
+/**
+ * Generates HTML strings to display the event's date and time with inline styles.
+ * @returns {Array<string>} - Array of HTML strings:
+ *   - **Index 0**: HTML string for the formatted date.
+ *   - **Index 1**: HTML string for the formatted time.
+ */
+  const formattedDateAndTimeHtml = computed(() => {
   const dateParts = formatDateAndTime(props.event.date, props.event.time)
   const formattedDateHtml = `<span style="font-weight: 800;">${dateParts.weekday}</span>, ${dateParts.day}. ${dateParts.month} ${dateParts.year}`
   const formattedTimeHtml = `<span style="font-weight: 800;">${dateParts.hour} ${dateParts.dayPeriod.toLowerCase()}</span>`
@@ -46,84 +51,85 @@ const formattedDateAndTimeHtml = computed(() => {
 .event-item {
   display: flex;
   flex-direction: row;
-  padding: 0 32px 32px 0;  
-  margin-bottom: 32px;
+  padding: 0 1.78rem 1.78rem 0;  
+  margin-bottom: 1.78rem;  
   overflow-x: visible;
   transition: all 0.5s ease;
 
   @media screen and (max-width: 600px) {
     display: block;
-    padding: 0 32px 24px 0;
+    padding: 0 1.78rem 1.33rem 0;  
   }
 }
 
 .event-item:hover {
   background-color: #DEDEDE;
-  padding: 16px 16px 32px 16px;
+  padding: 0.89rem 0.89rem 1.78rem 0.89rem;  
   transition: all 0.5s ease;
 
   @media screen and (max-width: 600px) {
-    padding-bottom: 0 0 24px 0;
+    padding-bottom: 0 0 1.33rem 0; 
   }
 }
 
 .info {
   flex-grow: 1;
-  margin-right: 32px;
+  margin-right: 1.78rem; 
 
   @media screen and (max-width: 600px) {
-    margin-bottom: 24px;
+    margin-bottom: 1.33rem; 
     margin-right: 0;
   }
 }
 
 .info-box {
-  margin: 8px 0;
+  margin: 0.44rem 0; 
 }
 
 .category {
   display: inline-block;
-  padding: 4px 8px;
+  padding: 0.22rem 0.44rem; 
   color: #FFFFFF;
   background-color: #000000;
   line-height: 100%;
-  font-size: 14px;
+  font-size: 0.78rem;  
 }
 
 .event-name {
   font-weight: 700;
-  font-size: 48px;
+  font-size: 2.67rem; 
 
   @media screen and (max-width: 600px) {
-    font-size: 32px;
+    font-size: 1.78rem; 
   }
 }
 
 .description {
-  font-size: 36px;  
+  font-size: 2rem; 
 
   @media screen and (max-width: 600px) {
-    font-size: 24px;
+    font-size: 1.33rem;  
   }
 }
 
 .date {
   font-weight: 300;
-  max-width: 250px;
+  max-width: 250px; 
   width: 100%;
-  margin-right: 32px;
+  margin-right: 1.78rem;  
   white-space: nowrap;
 
   @media screen and (max-width: 600px) {
     display: flex;
     flex-direction: row;
-    margin-bottom: 24px;
+    max-width: 100%;
+    margin-bottom: 1.33rem;  
   }
 }
 
 .day {
   @media screen and (max-width: 600px) {
-    margin-right: 8px;
+    margin-right: 0.44rem; 
   }
 }
 
@@ -131,11 +137,13 @@ const formattedDateAndTimeHtml = computed(() => {
   display: flex;
   flex-direction: row;
   justify-content: end;
-  max-width: 250px;
+  max-width: 250px; 
   width: 100%;
 
   @media screen and (max-width: 600px) {
     display: block;
+    max-width: 100%; 
+
   }
 }
 
@@ -145,15 +153,15 @@ button {
   background-color: #000000;
   color: #FFFFFF;
   border-width: 0;
-  margin-bottom: 8px;
+  margin-bottom: 0.44rem;  
   padding-inline: 0;
-  padding: 16px 24px;
+  padding: 0.89rem 1.33rem;  
   font: inherit;
   white-space: nowrap;
 
   @media screen and (max-width: 600px) {
     align-items: center;
-    padding: 8px 16px;
+    padding: 0.44rem 0.89rem; 
   }
 }
 
@@ -170,7 +178,7 @@ button {
   }
 }
 
-.nuxt-icon svg{
+.nuxt-icon svg {
   margin-bottom: 0;
   font-size: 1.5rem;
 }
--- utils/formatDateAndTime.ts
+++ utils/formatDateAndTime.ts
@@ -1,4 +1,23 @@
-export function formatDateAndTime(dateStr: string, timeStr = '12:00') {
+/**
+ * Formats a date and time into a structured object with readable parts.
+ *
+ * This function takes a date string and an optional time string, parses them into a `Date` object,
+ * and formats the date and time into a structured object using `Intl.DateTimeFormat`. The object includes
+ * components of the date and time such as the weekday, day, month, year, and time (hour).
+ *
+ * **Parameters**:
+ * - `dateStr` (string): The date in the format `YYYY-MM-DD` (e.g., "2024-07-20").
+ * - `timeStr` (string, optional): The time in the format `HH:mm` (e.g., "19:00"). Defaults to "12:00".
+ *
+ * **Returns**:
+ * - An object containing formatted parts of the date and time:
+ *   - `weekday`: Short name for the day of the week (e.g., "Wed").
+ *   - `day`: Two-digit day (e.g., "20").
+ *   - `month`: Full month name (e.g., "July").
+ *   - `year`: Four-digit year (e.g., "2024").
+ *   - `hour`: Numeric hour in 12-hour format (e.g., "8 PM").
+ */
+export function formatDateAndTime(dateStr: string, timeStr: string) {
   const date = new Date(`${dateStr}T${timeStr}:00`); // YYYY-MM-DDTHH:mm:ss
 
   // Define options for Intl.DateTimeFormat
